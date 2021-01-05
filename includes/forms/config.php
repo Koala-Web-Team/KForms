375129ee7773bbe64a2d11643adb76982a7afc01
AmrElabsy <amrelabsy55@gmail.com>
2021-01-06 00:08:37 +0200

    Fix Require Error

--- includes/forms/config.php
+++ includes/forms/config.php
@@ -2,13 +2,16 @@
     require_once("helper.php");
     require_once("KForm.php");
 	require_once("inputtypes/KInput.php");
+
+	require_once("inputtypes/checkable/KCheckables.php");
 	require_once("inputtypes/checkable/checkbox.php");
 	require_once("inputtypes/checkable/checkboxes.php");
-	require_once("inputtypes/checkable/KCheckables.php");
 	require_once("inputtypes/checkable/radioBotton.php");
+
 	require_once("inputtypes/numeric/KNumeric.php");
 	require_once("inputtypes/numeric/number.php");
 	require_once("inputtypes/numeric/range.php");
+
 	require_once("inputtypes/times/KTimes.php");
 	require_once("inputtypes/times/date.php");
 	require_once("inputtypes/times/dateTime.php");
