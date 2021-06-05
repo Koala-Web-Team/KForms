aa2c30721ee25ff9551be3ef1cc9855383be4971
AmrElabsy <amrelabsy55@gmail.com>
2021-06-05 20:41:59 +0200

    fix typos

--- includes/forms/elements/KValidation.php
+++ includes/forms/elements/KValidation.php
@@ -2,21 +2,20 @@
 
 class KValidation
 {
-    private $data;
-    private $rules;
-    private $errormsg;
-    private $errors = [];
-    private $validationStyle;
+	private $data;
+	private $rules;
+	private $errormsg;
+	private $errors = [];
+	private $validationStyle;
 
-    public function __construct($data, $rules, $errormsg = null, $validationStyle = "default")
-    {
-        $this->data = $data;
-        $this->rules = $rules;
-        $this->errormsg = $errormsg;
-        $this->validationStyle = $validationStyle;
-    }
-  
-  public function validate() {
+	public function __construct( $data, $rules, $errormsg = null, $validationStyle = "default" ) {
+		$this->data = $data;
+		$this->rules = $rules;
+		$this->errormsg = $errormsg;
+		$this->validationStyle = $validationStyle;
+	}
+
+	public function validate() {
 		try {
 			foreach ( $this->rules as $key => $keyRules ) {
 				$keyRules = $this->getKeyRules( $keyRules );
@@ -49,161 +48,56 @@ class KValidation
 		} catch ( ValidationException $e ) {
 			KoalaSessionHandler::init();
 			KoalaSessionHandler::setErrors( $this->errors );
-			header("Location: {$_SERVER['HTTP_REFERER']}");
+			header( "Location: {$_SERVER['HTTP_REFERER']}" );
 		} catch ( InvalidArgumentException $e ) {
 			KoalaSessionHandler::init();
 			KoalaSessionHandler::setErrors( [] );
-			header("Location: {$_SERVER['HTTP_REFERER']}");
+			header( "Location: {$_SERVER['HTTP_REFERER']}" );
 		}
 	}
-    private function getKeyRules($value)
-    {
-        if (is_array($value)) {
-            return $value;
-        } else {
-            return explode("|", $value);
-        }
-    }
 
-    private function required($key, $rulevalue = null, $errormsg = null)
-    {
-        $errormsg = $errormsg ?? "The $key is required";
-        if (!isset($this->data[$key]) || trim($this->data[$key]) == "") {
-            $this->errors[$key][] = $errormsg;
-        }
-    }
+	private function getKeyRules( $value ) {
+		if ( is_array( $value ) ) {
+			return $value;
+		} else {
+			return explode( "|", $value );
+		}
+	}
 
-    private function email($key, $rulevalue = null, $errormsg = null)
-    {
-        $errormsg = $errormsg ?? "$key is not a valid email address";
-        /**
-         * TODO: use Regular Expression
-         */
-        if (true) {
-            $this->errors[$key][] = $errormsg;
-        }
-    }
-
-    /**
-     * @throws ValidationException
-     */
-    private function min($key, $rulevalue = null, $errormsg = null)
-    {
-        //echo "test";
-        $errormsg = $errormsg ?? "The $key must be at least $rulevalue characters";
-        if ($rulevalue != null) {
-            if (trim(strlen($this->data[$key])) < $rulevalue) {
-                $this->errors[$key][] = $errormsg;
-            }
-        } else {
-            throw new ValidationException(/*$this->validator*/);
-        }
-    }
-
-    private function max($key, $rulevalue = null, $errormsg = null)
-    {
-        $errormsg = $errormsg ?? "The $key must be at most $rulevalue characters";
-        if ($rulevalue != null) {
-            if (trim(strlen($this->data[$key])) > $rulevalue) {
-                $this->errors[$key][] = $errormsg;
-            }
-        } else {
-            throw new ValidationException();
-        }
-    }
-
-
-    public function url($key, $rulevalue = null, $errormsg = null)
-    {
-        $errormsg = $errormsg ?? "$key Is Not A Valid URL";
-        if (!filter_var($this->data[$key], FILTER_VALIDATE_URL)) {
-            $this->errors[$key][] = $errormsg;
-        }
-    }
-
-    
-
-    private function tel($key, $errormsg = null)
-    {
-        $errormsg = $errormsg ?? "$key Must Be A Valid Phone Number";
-        $pattern='/(01)[0-9]{9}/';
-        if (!preg_match($pattern, $this->data[$key])) {
-            $this->errors[$key][] = $errormsg;
-        }
-    }
-
-
-
-    // public function in_array($key, $rulevalue, $errormsg = null)
-    // {
-    //     $errormsg = $errormsg ?? "$key Is Not In The Array [$rulevalue]";
-    //     $myArray=explode(",", $rulevalue);
-    //     if (!array_search($this->data[$key], $myArray)) {
-    //         $this->errors[$key][] = $errormsg;
-    //     }
-    // }
-
-    public function in_array($key,$rulevalue=null,$errormsg=null)
-    {
-        $errormsg = $errormsg ?? "$key Is Not In The Field [$rulevalue]";
-       
-        $myArray=explode(",",$rulevalue);
-
-        if(!array_search($this->data[$key],$myArray)){
-            $this->errors[$key][] = $errormsg;
-        }
-
-    }
-
-
-
-    public function in($key,$rulevalue,$errormsg = null ){
-        //in (input only two options (something,something else))
-        // in:something,something else;
-    	$errormsg = $errormsg ?? "$key Input Is Not In The Allowed Option [$rulevalue]";
-        $myArray=explode(",",$rulevalue);
-          	if(!array_search($this->data[$key],$myArray)||count($myArray)!==2){
-    	      $this->errors[$key][] = $errormsg;
- 
-              }
-    }	
-
-	public function not_in($key,$rulevalue, $errormsg = null ){
-		$errormsg = $errormsg ?? "$key Is In The Allowed Option [$rulevalue]";
-		$myArray=explode(",",$rulevalue);
-		if(array_search($this->data[$key],$myArray)){
+	private function required( $key, $rulevalue = null, $errormsg = null ) {
+		$errormsg = $errormsg ?? "The $key is required";
+		if ( !isset( $this->data[$key] ) || trim( $this->data[$key] ) == "" ) {
 			$this->errors[$key][] = $errormsg;
 		}
-		
 	}
 
-	private function min( $key, $rulevalue, $errormsg = null ) {
+	private function email( $key, $rulevalue = null, $errormsg = null ) {
+		$errormsg = $errormsg ?? "$key is not a valid email address";
+		/**
+		 * TODO: use Regular Expression
+		 */
+		if ( true ) {
+			$this->errors[$key][] = $errormsg;
+		}
+	}
+
+	/**
+	 * @throws ValidationException
+	 */
+	private function min( $key, $rulevalue = null, $errormsg = null ) {
 		//echo "test";
 		$errormsg = $errormsg ?? "The $key must be at least $rulevalue characters";
-	if ( $rulevalue != null ) {
+		if ( $rulevalue != null ) {
 			if ( trim( strlen( $this->data[$key] ) ) < $rulevalue ) {
 				$this->errors[$key][] = $errormsg;
 			}
 		} else {
-			throw new ValidationException();
-		}
-}
-
-  private function not_regex( $key, $rulevalue = null, $errormsg = null ) {
-		$errormsg = $errormsg ?? "The $key Must Not Match The Regex $rulevalue";
-		if ( $rulevalue != null ) {
-			if ( preg_match($rulevalue,$this->data[$key]) ) {
-				$this->errors[$key][] = $errormsg;
-			}
-		} else {
-			throw new ValidationException();
+			throw new ValidationException(/*$this->validator*/ );
 		}
 	}
-  
 
-	private function max( $key, $rulevalue, $errormsg = null ) {
+	private function max( $key, $rulevalue = null, $errormsg = null ) {
 		$errormsg = $errormsg ?? "The $key must be at most $rulevalue characters";
-$errormsg = $errormsg ?? "The $key must be at most $rulevalue characters";
 		if ( $rulevalue != null ) {
 			if ( trim( strlen( $this->data[$key] ) ) > $rulevalue ) {
 				$this->errors[$key][] = $errormsg;
@@ -212,13 +106,76 @@ $errormsg = $errormsg ?? "The $key must be at most $rulevalue characters";
 			throw new ValidationException();
 		}
 	}
-  
+
+	public function url( $key, $rulevalue = null, $errormsg = null ) {
+		$errormsg = $errormsg ?? "$key Is Not A Valid URL";
+		if ( !filter_var( $this->data[$key], FILTER_VALIDATE_URL ) ) {
+			$this->errors[$key][] = $errormsg;
+		}
+	}
+
+	private function tel( $key, $errormsg = null ) {
+		$errormsg = $errormsg ?? "$key Must Be A Valid Phone Number";
+		$pattern = '/(01)[0-9]{9}/';
+		if ( !preg_match( $pattern, $this->data[$key] ) ) {
+			$this->errors[$key][] = $errormsg;
+		}
+	}
+
+	// public function in_array($key, $rulevalue, $errormsg = null)
+	// {
+	//     $errormsg = $errormsg ?? "$key Is Not In The Array [$rulevalue]";
+	//     $myArray=explode(",", $rulevalue);
+	//     if (!array_search($this->data[$key], $myArray)) {
+	//         $this->errors[$key][] = $errormsg;
+	//     }
+	// }
+
+	public function in_array( $key, $rulevalue = null, $errormsg = null ) {
+		$errormsg = $errormsg ?? "$key Is Not In The Field [$rulevalue]";
+
+		$myArray = explode( ",", $rulevalue );
+
+		if ( !array_search( $this->data[$key], $myArray ) ) {
+			$this->errors[$key][] = $errormsg;
+		}
+	}
+
+	public function in( $key, $rulevalue, $errormsg = null ) {
+		//in (input only two options (something,something else))
+		// in:something,something else;
+		$errormsg = $errormsg ?? "$key Input Is Not In The Allowed Option [$rulevalue]";
+		$myArray = explode( ",", $rulevalue );
+		if ( !array_search( $this->data[$key], $myArray ) || count( $myArray ) !== 2 ) {
+			$this->errors[$key][] = $errormsg;
+		}
+	}
+
+	public function not_in( $key, $rulevalue, $errormsg = null ) {
+		$errormsg = $errormsg ?? "$key Is In The Allowed Option [$rulevalue]";
+		$myArray = explode( ",", $rulevalue );
+		if ( array_search( $this->data[$key], $myArray ) ) {
+			$this->errors[$key][] = $errormsg;
+		}
+	}
+
+	private function not_regex( $key, $rulevalue = null, $errormsg = null ) {
+		$errormsg = $errormsg ?? "The $key Must Not Match The Regex $rulevalue";
+		if ( $rulevalue != null ) {
+			if ( preg_match( $rulevalue, $this->data[$key] ) ) {
+				$this->errors[$key][] = $errormsg;
+			}
+		} else {
+			throw new ValidationException();
+		}
+	}
+
 	private function regex( $key, $rulevalue = null, $errormsg = null ) {
 		$errormsg = $errormsg ?? "The $key Must  Match The Regex $rulevalue";
 		if ( $rulevalue != null ) {
-			if (!preg_match($rulevalue,$this->data[$key]) ) {
+			if ( !preg_match( $rulevalue, $this->data[$key] ) ) {
 				$this->errors[$key][] = $errormsg;
-			} 
+			}
 		}
 	}
 
@@ -243,20 +200,20 @@ $errormsg = $errormsg ?? "The $key must be at most $rulevalue characters";
 	private function date_format( $key, $ruleValue, $errormsg = null ) {
 		$errormsg = $errormsg ?? "The $key must be match the format: $ruleValue";
 
-		if (! is_string($this->data[$key]) && ! is_numeric($this->data[$key])) {
+		if ( !is_string( $this->data[$key] ) && !is_numeric( $this->data[$key] ) ) {
 			$this->errors[$key][] = $errormsg;
 			return;
 		}
 
 		$format = $ruleValue;
 
-		$date = DateTime::createFromFormat('!'.$format, $this->data[$key]);
-		if (!($date && $date->format($format) == $this->data[$key])) {
+		$date = DateTime::createFromFormat( '!' . $format, $this->data[$key] );
+		if ( !($date && $date->format( $format ) == $this->data[$key]) ) {
 			$this->errors[$key][] = $errormsg;
 		}
 	}
 
-	private function file($key, $ruleValue = null, $errormsg = null) {
+	private function file( $key, $ruleValue = null, $errormsg = null ) {
 		$errormsg = $errormsg ?? "The $key must be a Valid File";
 		$file = $_FILES[$key];
 		if ( !file_exists( $file['tmp_name'] ) || $file['size'] == 0 ) {
